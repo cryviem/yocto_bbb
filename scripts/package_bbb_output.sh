af55a7d5a63b007a0de31b443eb35a5a07696641
AndyNguyen <andynguyen@axon.com>
2024-04-21 17:43:20 +0700

    decorate scripts/package_bbb_output.sh

--- scripts/package_bbb_output.sh
+++ scripts/package_bbb_output.sh
@@ -1,4 +1,9 @@
 # To pack the output
+# This script assume:
+#   Run on root: yocto_bbb
+#   Machine is beaglebone-yocto
+#   Image is joe-image-full
+
 YOCTO_BBB=`pwd`
 MACHINE=beaglebone-yocto
 IMAGE_DIR=${YOCTO_BBB}/build/tmp/deploy/images/${MACHINE}
@@ -10,9 +15,18 @@ DTB=am335x-boneblack.dtb
 ROOTFS=joe-image-full-beaglebone-yocto.tar.bz2
 MODULES=modules-beaglebone-yocto.tgz
 
+show_error() {
+    echo "---- This script has some limitations ---"
+    echo "  Must run on root: yocto_bbb"
+    echo "  Machine is beaglebone-yocto only"
+    echo "  Image is joe-image-full only"
+    echo "-----------------------------------------"
+}
+
 if [ ! -d ${IMAGE_DIR} ]
 then
-    echo "${IMAGE_DIR} not found, please run this script on root"
+    echo "${IMAGE_DIR} not found!"
+    show_error
     exit 1
 fi
 
@@ -20,36 +34,42 @@ fi
 if [ ! -f ${IMAGE_DIR}/${SBL} ]
 then
     echo "${IMAGE_DIR}/${SBL} not found"
+    show_error
     exit 1
 fi
 
 if [ ! -f ${IMAGE_DIR}/${UBOOT} ]
 then
     echo "${IMAGE_DIR}/${UBOOT} not found"
+    show_error
     exit 1
 fi
 
 if [ ! -f ${IMAGE_DIR}/${KERNEL} ]
 then
     echo "${IMAGE_DIR}/${KERNEL} not found"
+    show_error
     exit 1
 fi
 
 if [ ! -f ${IMAGE_DIR}/${DTB} ]
 then
     echo "${IMAGE_DIR}/${DTB} not found"
+    show_error
     exit 1
 fi
 
 if [ ! -f ${IMAGE_DIR}/${ROOTFS} ]
 then
     echo "${IMAGE_DIR}/${ROOTFS} not found"
+    show_error
     exit 1
 fi
 
 if [ ! -f ${IMAGE_DIR}/${MODULES} ]
 then
     echo "${IMAGE_DIR}/${MODULES} not found"
+    show_error
     exit 1
 fi
 
