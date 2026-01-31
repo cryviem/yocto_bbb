bc452ecb7897a9b2dc3811a4d8648d8695bbffd4
AndyNguyen <andynguyen@axon.com>
2026-01-31 22:13:58 +0700

    fix terminal_over_acm.sh

--- meta-joe/recipes-core/joe-init/files/terminal_over_acm.sh
+++ meta-joe/recipes-core/joe-init/files/terminal_over_acm.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-ACM_DEVICE=/dev/ttyGS0
+ACM_DEV=ttyGS0
+ACM_DEV_PATH=/dev/${ACM_DEV}
 
 # Wait for device to appear
 for i in $(seq 1 10); do
-    if [ -c $ACM_DEVICE ]; then
+    if [ -c $ACM_DEV_PATH ]; then
         break
     fi
     sleep 1
 done
 
-if [ ! -c $ACM_DEVICE ]; then
-    echo "$ACM_DEVICE not found"
+if [ ! -c $ACM_DEV_PATH ]; then
+    echo "$ACM_DEV_PATH not found"
     exit 1
 fi
 
 # Run getty in a respawn loop (background)
 while true; do
-    getty -L $ACM_DEVICE 115200 vt100
+    getty -L $ACM_DEV 115200 vt100
     sleep 1
-done &
+done
